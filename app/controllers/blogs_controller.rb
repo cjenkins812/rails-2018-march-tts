0da3a2a80a6073d8107162796bcc95b7765717cd
Emily Ring <btyy77c@gmail.com>
2018-04-09 05:55:16 -0400

    Added routes and controllers

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,19 @@
+class BlogsController < ApplicationController
+  # Load Data here.  We'll learn this later
+
+  def edit
+  end
+
+  def index
+  end
+
+  def josh
+    render 'joshkongindex'
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #Custom routes.  We'll update these after we learn ruby
+
+  get '/blogs' => 'blogs#index'
+  get '/blog_show' => 'blogs#show'
+  get '/blog_josh' => 'blogs#josh'
+  get '/blogs/new' => 'blogs#new'
+  get '/blogs/edit' => 'blogs#edit'
+
+
+  get '/users' => 'users#index'
+  get '/user_show' => 'users#show'
+  get '/users/new' => 'users#new'
+  get '/users/edit' => 'users#edit'
 end
